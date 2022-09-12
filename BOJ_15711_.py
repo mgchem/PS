af47f37076b017b9a14a216af73e263d02959d2f
Myeonggyun Choi <37331594+mgchem@users.noreply.github.com>
2022-09-12 15:53:57 +0900

    Create BOJ_15711_.py

---
+++ BOJ_15711_.py
@@ -0,0 +1,23 @@
+import sys
+T = int(sys.stdin.readline())
+
+def isPrime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, int(n**0.5)+1):
+            if n%i == 0:
+                return False
+        return True
+
+for i in range(T):
+    A, B = map(int, sys.stdin.readline().split())
+    if A + B == 2 or A + B == 3:
+        print("NO")
+    elif (A + B) % 2 == 0:
+        print("YES")
+    else:
+        if isPrime(A+B - 2):
+            print("YES")
+        else:
+            print("NO")
