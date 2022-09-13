4ebea92fd8c164514134c4bac886ce02cd2edc2b
Myeonggyun Choi <37331594+mgchem@users.noreply.github.com>
2022-09-13 20:45:34 +0900

    Create BOJ_2920.py

---
+++ BOJ_2920.py
@@ -0,0 +1,8 @@
+import sys
+data = list(map(int, sys.stdin.readline().split()))
+if data == [1, 2, 3, 4, 5, 6, 7, 8]:
+    print("ascending")
+elif data == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print("descending")
+else:
+    print("mixed")
