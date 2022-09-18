78d0990f28ca66b73b048239baa1d78f5d58193c
Myeonggyun Choi <37331594+mgchem@users.noreply.github.com>
2022-09-18 10:14:09 +0900

    Update BOJ_1181_.py

--- BOJ_1181_.py
+++ BOJ_1181_.py
@@ -1,17 +1,32 @@
-#TLE
+#Quick sort
+
 import sys
 n = int(sys.stdin.readline())
 data = []
 for i in range(n):
     data.append(sys.stdin.readline().strip())
 data = list(set(data))
-n = len(data)
-for i in range(n-1):
-    for j in range(i+1, n):
-        if len(data[i]) > len(data[j]):
-            data[i], data[j] = data[j], data[i]
-        elif len(data[i]) == len(data[j]):
-            if data[i] > data[j]:
-                data[i], data[j] = data[j], data[i]
-for w in data:
+
+def qsort(arr):
+    def sort(low, high):
+        if high <= low:
+            return
+        mid = partition(low, high)
+        sort(low, mid - 1)
+        sort(mid, high)
+    def partition(low, high):
+        pivot = arr[(low + high) // 2]
+        while low <= high:
+            while len(arr[low]) < len(pivot) or (len(arr[low]) == len(pivot) and arr[low] < pivot):
+                low += 1
+            while len(arr[high]) > len(pivot) or (len(arr[high]) == len(pivot) and arr[high] > pivot):
+                high -= 1
+            if low <= high:
+                arr[low], arr[high] = arr[high], arr[low]
+                low, high = low + 1, high - 1
+        return low
+    sort(0, len(arr) - 1)
+    return arr
+
+for w in qsort(data):
     print(w)
