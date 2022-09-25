10bfa6dd423fff390cec6a63bc866201fa01a162
Myeonggyun Choi <37331594+mgchem@users.noreply.github.com>
2022-09-25 14:49:49 +0900

    Create BOJ_10828.py

---
+++ BOJ_10828.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+def main(n):
+    stack = []
+    for i in range(n):
+        s = sys.stdin.readline().strip()
+        if "push" in s:
+            stack.append(int(s[5::]))
+        elif "pop" in s:
+            if len(stack) == 0:
+                print(-1)
+            else:
+                print(stack[-1])
+                stack = stack[:-1:]
+        elif "size" in s:
+            print(len(stack))
+        elif "empty" in s:
+            if len(stack) == 0:
+                print(1)
+            else:
+                print(0)
+        else:
+            if len(stack) == 0:
+                print(-1)
+            else:
+                print(stack[-1])
+
+main(n)
